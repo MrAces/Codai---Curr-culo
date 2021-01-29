46bd92b7f9c923e71634ac3e443883f7b8ab822b
MrAces <77990359+MrAces@users.noreply.github.com>
2021-01-29 16:52:17 -0300

    Limpei o JS

--- js/index.js
+++ js/index.js
@@ -1,22 +1,9 @@
-var nome = "André";
-var cargo = "Desenvolvedor";
+//Var dos Botões
 
-var nomeHtml = document.getElementById("nome-html");
-var cargoHtml =document.getElementById("cargo-html");
 var text1 = document.getElementById("text-1");
 var text2 = document.getElementById("text-2");
 
-function colocarNomeNoHtml(nome){
-nomeHtml.innerHTML = nome;
-}
-
-function colocarCargo(cargo){
-cargoHtml.innerHTML = cargo;
-}
-
-function logarNome() {
-    console.log(nome);
-}
+//Config Botão Projetos
 
 function clickProjetos() {
     console.log ("Clicou no Botão Projetos");
@@ -24,11 +11,10 @@ function clickProjetos() {
     text1.style.display = "none";
 }
 
+//Config Botão Sobre 
+
 function clickSobre() {
     console.log ("Clicou no Botão Sobre");
     text1.style.display = "block";
     text2.style.display = "none";
 }
-
-colocarNomeNoHtml(nome);
-colocarCargo(cargo);
