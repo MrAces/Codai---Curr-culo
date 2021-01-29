bb725fa7211d505bfe0589bdcf32b966a48525eb
MrAces <77990359+MrAces@users.noreply.github.com>
2021-01-29 15:10:05 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var nome = "André Cezar";
+var cargo = "Desenvolvedor";
+
+var nomeHtml = document.getElementById("nome-html");
+var cargoHtml =document.getElementById("cargo-html");
+var text1 = document.getElementById("text-1");
+var text2 = document.getElementById("text-2");
+
+function colocarNomeNoHtml(nome){
+nomeHtml.innerHTML = nome;
+}
+
+function colocarCargo(cargo){
+cargoHtml.innerHTML = cargo;
+}
+
+function logarNome() {
+    console.log(nome);
+}
+
+function clickProjetos() {
+    console.log ("Clicou no Botão Projetos");
+    text2.style.display = "block";
+    text1.style.display = "none";
+}
+
+function clickSobre() {
+    console.log ("Clicou no Botão Sobre");
+    text1.style.display = "block";
+    text2.style.display = "none";
+}
+
+colocarNomeNoHtml(nome);
+colocarCargo(cargo);
